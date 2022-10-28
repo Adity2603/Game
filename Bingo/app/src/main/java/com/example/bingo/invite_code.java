7f6322adf9687a2917c41b970082ae30f5836d9b
Aditya2603 <amddeshmukh26@gmail.com>
2022-10-28 11:49:17 +0530

    invite code layout complete

--- Bingo/app/src/main/java/com/example/bingo/invite_code.java
+++ Bingo/app/src/main/java/com/example/bingo/invite_code.java
@@ -10,5 +10,8 @@ public class invite_code extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_code);
+        getSupportActionBar().hide();
+
+
     }
 }
